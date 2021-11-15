8fde25c5b19f38168efba43e45ae0934a5386887
heycn <x1chennan@qq.com>
2021-11-15 19:51:49 +0800

    优化代码

--- src/test.js
+++ src/test.js
@@ -1,48 +1,65 @@
 import string from './css.js'
-let n = 1
-demo.innerText = string.substr(0, n)
-demo2.innerHTML = string.substr(0, n)
 
-let time = 80
-
-let run = () => {
-  n += 1
-  if (n > string.length) {
-    window.clearInterval(id)
-    return
-  }
-  demo.innerText = string.substr(0, n)
-  demo2.innerHTML = string.substr(0, n)
-  demo.scrollTop = demo.scrollHeight
+const player = {
+  id: undefined,
+  time: 60,
+  ui: {
+    demo: document.querySelector('#demo'),
+    demo2: document.querySelector('#demo2'),
+  },
+  events: {
+    '#btnPause': 'pause',
+    '#btnPlay': 'play',
+    '#btnSlow': 'slow',
+    '#btnNormal': 'normal',
+    '#btnFast': 'fast',
+  },
+  n: 1,
+  init: () => {
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.bindEvents()
+    player.play()
+  },
+  bindEvents: () => {
+    for (let key in player.events) {
+      if (player.events.hasOwnProperty) {
+        const value = player.events[key]
+        document.querySelector(key).onclick = player[value]
+      }
+    }
+  },
+  run: () => {
+    player.n += 1
+    if (player.n > string.length) {
+      window.clearInterval(player.id)
+      return
+    }
+    player.ui.demo.innerText = string.substr(0, player.n)
+    player.ui.demo2.innerHTML = string.substr(0, player.n)
+    player.ui.demo.scrollTop = player.ui.demo.scrollHeight
+  },
+  play: () => {
+    player.id = setInterval(player.run, player.time)
+  },
+  pause: () => {
+    window.clearInterval(player.id)
+  },
+  slow: () => {
+    player.pause()
+    player.time = 200
+    player.play()
+  },
+  normal: () => {
+    player.pause()
+    player.time = 60
+    player.play()
+  },
+  fast: () => {
+    player.pause()
+    player.time = 0
+    player.play()
+  },
 }
 
-const play = () => {
-  return setInterval(run, time)
-}
-const pause = () => {
-  window.clearInterval(id)
-}
-
-let id = play()
-
-btnPause.onclick = () => {
-  pause()
-}
-btnPlay.onclick = () => {
-  id = play()
-}
-btnSlow.onclick = () => {
-  pause()
-  time = 200
-  id = play()
-}
-btnNormal.onclick = () => {
-  pause()
-  time = 80
-  id = play()
-}
-btnFast.onclick = () => {
-  pause()
-  time = 0
-  id = play()
-}
+player.init()
